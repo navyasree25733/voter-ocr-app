d637e9193786b970d7af16da79374edb2ea87b54
navyasree-blip <navya.sree@imaginemarketingindia.com>
2026-01-30 15:39:20 +0530

    Requirement change

--- security.py
+++ security.py
@@ -14,16 +14,30 @@
 
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-MAX_LEN = 72  # bcrypt byte limit
-
-def _normalize_password(password: str) -> bytes:
-    return password.strip().encode("utf-8")[:MAX_LEN]
+pwd_context = CryptContext(
+    schemes=["argon2"],
+    deprecated="auto"
+)
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(_normalize_password(password))
+    return pwd_context.hash(password)
 
 def verify_password(password: str, hashed: str) -> bool:
-    return pwd_context.verify(_normalize_password(password), hashed)
+    return pwd_context.verify(password, hashed)
+
+
+# from passlib.context import CryptContext
+
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# MAX_LEN = 72  # bcrypt byte limit
+
+# def _normalize_password(password: str) -> bytes:
+#     return password.strip().encode("utf-8")[:MAX_LEN]
+
+# def hash_password(password: str) -> str:
+#     return pwd_context.hash(_normalize_password(password))
+
+# def verify_password(password: str, hashed: str) -> bool:
+#     return pwd_context.verify(_normalize_password(password), hashed)
 
