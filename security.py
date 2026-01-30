20cde50b19b066f033319f52823454fb362e303a
navyasree-blip <navya.sree@imaginemarketingindia.com>
2026-01-30 17:23:44 +0530

    Update

--- security.py
+++ security.py
@@ -1,14 +1,26 @@
+# from passlib.context import CryptContext
+
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# MAX_LEN = 72  # bcrypt limit
+
+# def hash_password(password: str) -> str:
+#     password = password.strip()[:MAX_LEN]
+#     return pwd_context.hash(password)
+
+# def verify_password(password: str, hashed: str) -> bool:
+#     password = password.strip()[:MAX_LEN]
+#     return pwd_context.verify(password, hashed)
+
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-MAX_LEN = 72  # bcrypt limit
+pwd_context = CryptContext(
+    schemes=["argon2"],
+    deprecated="auto"
+)
 
 def hash_password(password: str) -> str:
-    password = password.strip()[:MAX_LEN]
     return pwd_context.hash(password)
 
 def verify_password(password: str, hashed: str) -> bool:
-    password = password.strip()[:MAX_LEN]
     return pwd_context.verify(password, hashed)
-
