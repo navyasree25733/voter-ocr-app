2b703686ce41cff6cad1961ffe3b7569f2d33f33
navyasree-blip <navya.sree@imaginemarketingindia.com>
2026-01-30 11:34:32 +0530

    Add MySQL connector

--- db.py
+++ db.py
@@ -1,20 +1,44 @@
+# import os
 # import mysql.connector
 
 # def get_db():
 #     return mysql.connector.connect(
-#         host="localhost",
-#         user="root",
-#         password="123456789@",        # 🔴 change
-#         database="voter_ocr_db"
+#         host=os.getenv("MYSQL_HOST"),
+#         user=os.getenv("MYSQL_USER"),
+#         password=os.getenv("MYSQL_PASSWORD"),
+#         database=os.getenv("MYSQL_DATABASE"),
+#         port=int(os.getenv("MYSQL_PORT", 3306)),
 #     )
+
 import os
 import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
+
+# # import mysql.connector
+
+# # def get_db():
+# #     return mysql.connector.connect(
+# #         host="localhost",
+# #         user="root",
+# #         password="123456789@",        # 🔴 change
+# #         database="voter_ocr_db"
+# #     )
+# import os
+# import mysql.connector
+
+# def get_db():
+#     return mysql.connector.connect(
+#         host=os.getenv("DB_HOST"),
+#         user=os.getenv("DB_USER"),
+#         password=os.getenv("DB_PASSWORD"),
+#         database=os.getenv("DB_NAME"),
+#         port=int(os.getenv("DB_PORT", 3306))
+#     )
